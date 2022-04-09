3ed0aac11b0aecff65c4a0f1e39e43e9e6811d10
michaelfarias <michaelfarias01@gmail.com>
2022-04-09 17:45:12 -0300

    criando email de lembrete

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -3,13 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
 import { CommitmentRepository } from 'src/commitments/commitments.repository';
 import { TaskService } from './task.service';
+import { SendgridService } from '../sendgrid/sendgrid.service';
 
 @Module({
     imports: [
         ScheduleModule.forRoot(),
         TypeOrmModule.forFeature([CommitmentRepository])],
     providers: [
-        TaskService],
+        TaskService,
+        SendgridService
+    ],
     controllers: []
 })
 export class TaskModule { }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -3,11 +3,14 @@ import { Cron } from '@nestjs/schedule';
 import { InjectRepository } from "@nestjs/typeorm";
 import { Commitment } from "src/commitments/commitment.entity";
 import { CommitmentRepository } from '../commitments/commitments.repository';
+import { SendgridService } from '../sendgrid/sendgrid.service';
 
 @Injectable()
 export class TaskService {
-    constructor(@InjectRepository(CommitmentRepository)
-    private commitmentRepository: CommitmentRepository
+    constructor(
+        @InjectRepository(CommitmentRepository)
+        private commitmentRepository: CommitmentRepository,
+        private readonly sendgridService: SendgridService
     ) { }
 
     @Cron('*/60 * * * * *')
@@ -22,8 +25,24 @@ export class TaskService {
     }
 
     private async sendReminder(commitments: Commitment[]) {
-        commitments.forEach(commitment => {
-            const { email_people_involved } = commitment;
+        commitments.forEach(async (commitment) => {
+            const { email_people_involved, description, date_time, place } = commitment;
+
+            const mail = {
+                to: email_people_involved,
+                from: 'noreply@application.com',
+                subject: 'REMINDER',
+                text: 'Appointment Reminder',
+                html: '<body>' +
+                    `<h3>Appointment Date: ${date_time}</h3></br>` +
+                    `<h3>Appointment Description: ${description}</h3></br>` +
+                    `<h3>Place: ${place}</h3></br>` +
+                    `<h3>People Involved: ${email_people_involved}</h3></br>` +
+                    '</body>'
+            };
+
+            // await this.sendgridService.send(mail)
+
             console.log(email_people_involved, commitment)
             console.log('######################################')
         });
